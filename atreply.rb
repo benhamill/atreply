d328304490046bbf55c69920d23b5096791c668b
Ben Hamill <git-commits@benhamill.com>
2009-02-05 21:38:45 -0600

    Actual working foray.

--- atreply.rb
+++ atreply.rb
@@ -6,9 +6,7 @@ require 'twitter'
 class Reply
   attr_accessor :text, :author, :in_reply_to, :time
   
-  def initialize status_id
-    
-    
+  def initialize status
     self.text = status.text
     self.author = if status.user.name then status.user.name else status.user.screen_name end
     self.time = status.created_at
