09cf859565e23dbafd3100a655a77c5f6e535e7a
Ben Hamill <git-commits@benhamill.com>
2009-02-05 21:22:51 -0600

    Initial, crappy foray. Proof of concept.

--- atreply.rb
+++ atreply.rb
@@ -1,4 +1,17 @@
 #!/usr/bin/env ruby
 
+require 'rubygems'
+require 'twitter'
+
 class Reply
+  attr_accessor :text, :author, :in_reply_to, :time
+  
+  def initialize status_id
+    
+    
+    self.text = status.text
+    self.author = if status.user.name then status.user.name else status.user.screen_name end
+    self.time = status.created_at
+    self.in_reply_to = status.in_reply_to_status_id
+  end
 end
