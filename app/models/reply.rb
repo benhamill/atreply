deca9a12befa75dce9bd1ba1a417ddfacefb34e9
Ben Hamill <ben@Hermes.(none)>
2009-02-14 23:22:33 -0600

    Reply code worth keeping.

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -13,24 +13,5 @@ class Reply
     self.author = if status.user.name then status.user.name else status.user.screen_name end
     self.time = status.created_at
     self.in_reply_to = status.in_reply_to_status_id
-    self.atreply = Reply.new self.in_reply_to unless self.in_reply_to.nil?
-  end
-  
-  def each_reply &block
-    reply_chain.each do |reply|
-      yield reply
-    end
-  end
-  
-  def to_s
-    self.author + ' - ' + self.time.to_s + "\n" + self.text
-  end
-  
-  protected
-  
-  def reply_chain
-    return [self] unless self.atreply
-    
-    self.atreply.reply_chain << self
   end
 end
