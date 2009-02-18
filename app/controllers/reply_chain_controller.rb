a1478941fe13dba743be8b4b1cc5aeda449c2ea1
Ben Hamill <ben@Hermes.(none)>
2009-02-17 21:59:55 -0600

    Testing simpler action/view logic.

--- app/controllers/reply_chain_controller.rb
+++ app/controllers/reply_chain_controller.rb
@@ -1,5 +1,6 @@
 class ReplyChainController < ApplicationController
   def display
-    @reply_chain = ReplyChain.new(params[:id])
+    @reply_chain = [1, 2, 3]
+    #@reply_chain = ReplyChain.new(params[:id])
   end
 end
