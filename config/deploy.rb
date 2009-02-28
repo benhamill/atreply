7d2c707fcc39ef3421cf55e63f92ec92e7fc3818
Ben Hamill <ben@Hermes.(none)>
2009-02-27 18:09:24 -0600

    Adjust config/deploy.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,26 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "atreply"
+set :repository,  "git clone git://github.com/BenHamill/atreply.git"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "~/apps/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :git
+default_run_options[:pty] = true
+set :deploy_via, :remote_cache
+set :branch, 'master'
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+#role :app, "your app-server here"
+#role :web, "your web-server here"
+#role :db,  "your db-server here", :primary => true
+server 'atreply.benhamill.com', :app, :web, :db, :primary => true
+
+namespace :deploy do
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
