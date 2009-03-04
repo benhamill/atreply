440df0f0f813c5c8c6b91a3c4dbbd6949765aa89
Ben Hamill <ben@Hermes.(none)>
2009-03-03 22:07:42 -0600

    Config gem environment for DH.

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,9 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 
+#For Passenger on DreamHost:
+ENV['GEM_PATH'] = '/home/hamillbd/.gems/'
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
