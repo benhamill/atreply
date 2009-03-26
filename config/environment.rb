14dc1cd608142910ff1de273e826d59889eb6784
Ben Hamill <git-commits@benhamill.com>
2009-03-26 18:20:40 -0500

    Switching to Grackle for Twitter API.

--- config/environment.rb
+++ config/environment.rb
@@ -30,7 +30,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'joshuamiller-twitter4r', :lib => 'twitter', :source => 'http://gems.github.com'
+  config.gem 'hayesdavis-grackle', :lib => 'grackle', :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
