a319b70d4bc61fe26eeef5b0651236734c89a22c
Ben Hamill <git-commits@benhamill.com>
2009-03-26 19:41:53 -0500

    Generate the twitter user config on deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,3 +21,22 @@ namespace :deploy do
     run "touch #{current_path}/tmp/restart.txt"
   end
 end
+
+desc 'Creates the production twitter_user.yml up.'
+task :setup_production_twitter_user_config do
+  username = Capistrano::CLI.password_prompt 'Twitter Username:'
+  password = Capistrano::CLI.password_prompt 'Twitter Password:'
+  
+  require 'yaml'
+  config = { :username=>username, :password=>password }
+  
+  run "mkdir -p #{shared_path}/config"
+  put spec.to_yaml, "#{shared_path}/config/twitter_user.yaml"
+end
+after 'deploy:setup', :setup_production_twitter_user_config
+
+desc 'Carries twitter_user.yml forward on update.'
+task :copy_production_twitter_user_config do
+  run "cp #{shared_path}/config/twitter_user.yaml #{release_path}/config/twitter_user.yaml"
+end
+after 'deploy:update_code', :copy_production_twitter_user_config
