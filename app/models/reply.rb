6400ec77778dabe8d44cdeefdd0780530876e584
Ben Hamill <git-commits@benhamill.com>
2009-03-26 19:35:10 -0500

    Convert to Grackle and load user data from yaml.

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -3,9 +3,7 @@
 class Reply
   attr_accessor :text, :author, :in_reply_to, :time, :id, :screen_name
   
-  def initialize status_id
-    client = self.login
-    
+  def initialize client, status_id
     status = client.statuses.show.json? :id => status_id
     
     self.id = status_id
@@ -14,11 +12,5 @@ class Reply
     self.screen_name = status.user.screen_name
     self.time = status.created_at
     self.in_reply_to = status.in_reply_to_status_id
-  end
-  
-  protected
-  
-  def login
-    Grackle::Client.new :username => 'atreply_net', :password => 
   end
 end
--- app/models/reply_chain.rb
+++ app/models/reply_chain.rb
@@ -1,13 +1,19 @@
 # for testing use id 1181051952
 
+require 'yaml'
+
 class ReplyChain
   attr_accessor :chain
   
+  TWITTER_CONFIG_FILE = "#{RAILS_ROOT}/config/twitter_user.yaml"
+  
   def initialize status_id
-    self.chain = [Reply.new(status_id.to_s)]
+    client = self.login
+    
+    self.chain = [Reply.new(client, status_id.to_s)]
     
     while @chain[-1].in_reply_to
-      @chain << Reply.new(@chain[-1].in_reply_to)
+      @chain << Reply.new(client, @chain[-1].in_reply_to)
     end
     
     @chain.reverse!
@@ -21,5 +27,13 @@ class ReplyChain
     @chain.each do |reply|
       yield reply
     end
+  end
+  
+  protected
+  
+  def login
+    config = YAML::load_file TWITTER_CONFIG_FILE
+    
+    Grackle::Client.new config
   end
 end
